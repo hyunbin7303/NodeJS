a39f30cae833ee9abf2c7f86fe0835d530b9d420
hyunbin7303 <hyunbin7303@gmail.com>
2019-04-30 15:29:08 -0400

    Add

--- NodeJsStart/App.js
+++ NodeJsStart/App.js
@@ -1,24 +1,23 @@
 var express = require('express');
 var app = express(); // able to access
+var bodyParser = require('body-parser');
+var UrlencodedParser = bodyParser.urlencoded({extended: false});
+
 
 app.set('view engine', 'ejs');
-
-
-
+app.use('/assets',express.static('stuff'));
 app.get('/', function(req,res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 app.get('/contact', function(req,res){
-    res.sendFile(__dirname + '/contact.html');
+    res.render('contact', {qs:req.query});
 });
-
-
-
+app.post('/contact', UrlencodedParser, function(req,res){
+    console.log(req.body);
+    res.render('contact-success', {data: req.body});
+});
 app.get('/profile/:name', function(req,res){
-
-
-  var data = {age: 28, job: 'software engineer'};
-
+  var data = {age: 28, job: 'software engineer', hobbies: ['breakdance', 'programming', 'something']};
   res.render('profile', {person: req.params.name, data:data});
 });
 
